63811c36b3ed978d0b3b3e76ec7e37ed614431f4
SantiagoBonillaGuevara <sbonilla284@gmail.com>
2025-02-27 00:09:05 -0500

    inyeccion de dependencias

--- app/src/main/java/com/example/horoscapp/data/providers/HoroscopeProvider.kt
+++ app/src/main/java/com/example/horoscapp/data/providers/HoroscopeProvider.kt
@@ -2,8 +2,9 @@ package com.example.horoscapp.data.providers
 
 import com.example.horoscapp.domain.model.HoroscopeInfo
 import com.example.horoscapp.domain.model.HoroscopeInfo.*
+import javax.inject.Inject
 
-class HoroscopeProvider {
+class HoroscopeProvider @Inject constructor() {
     fun getHoroscopes(): List<HoroscopeInfo> {
         return listOf(
             Aries,
