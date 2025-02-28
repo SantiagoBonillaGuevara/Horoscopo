2c97992800d9cb64157683d2f589b198df6d6cc4
SantiagoBonillaGuevara <sbonilla284@gmail.com>
2025-02-27 21:19:04 -0500

    horoscope detail viewmodel

--- app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
