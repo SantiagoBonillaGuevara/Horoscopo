8e3a36c5c94e7289559d64fb908638e85a124ecb
SantiagoBonillaGuevara <sbonilla284@gmail.com>
2025-03-02 22:36:19 -0500

    implement loading, success, and error states in HoroscopeDetailActivity

--- app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,9 +3,14 @@ package com.example.horoscapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.example.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -19,6 +24,37 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        is HoroscopeDetailState.Error -> errorState()
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Success -> successState()
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState() {
+        TODO("Not yet implemented")
+    }
+
+    private fun errorState() {
+        TODO("Not yet implemented")
+    }
+
+    private fun loadingState() {
+        binding.pbDetail.isVisible = true
     }
 }
