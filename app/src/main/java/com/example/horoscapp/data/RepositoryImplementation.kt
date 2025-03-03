ae111283a8c5a16d4515df3958dd4a8f6a5325d2
SantiagoBonillaGuevara <sbonilla284@gmail.com>
2025-03-03 00:05:38 -0500

    refactor repository to use PredictionModel and update response handling

--- app/src/main/java/com/example/horoscapp/data/RepositoryImplementation.kt
+++ app/src/main/java/com/example/horoscapp/data/RepositoryImplementation.kt
@@ -2,16 +2,16 @@ package com.example.horoscapp.data
 
 import android.util.Log
 import com.example.horoscapp.data.network.HoroscopeApiService
-import com.example.horoscapp.data.network.response.PredictionResponse
 import com.example.horoscapp.domain.Repository
+import com.example.horoscapp.domain.model.PredictionModel
 import javax.inject.Inject
 
 class RepositoryImplementation @Inject constructor(private val apiService: HoroscopeApiService) :
     Repository {
 
-    override suspend fun getPrediction(sign: String): PredictionResponse? {
+    override suspend fun getPrediction(sign: String): PredictionModel? {
         runCatching { apiService.getHoroscope(sign) }
-            .onSuccess { return it }
+            .onSuccess { return it.toDomain() }
             .onFailure { Log.i("santiago", "ha ocurrido un error ${it.message}") }
         return null
     }
--- app/src/main/java/com/example/horoscapp/domain/Repository.kt
+++ app/src/main/java/com/example/horoscapp/domain/Repository.kt
@@ -1,7 +1,7 @@
 package com.example.horoscapp.domain
 
-import com.example.horoscapp.data.network.response.PredictionResponse
+import com.example.horoscapp.domain.model.PredictionModel
 
 interface Repository {
-    suspend fun getPrediction(sign: String): PredictionResponse?
+    suspend fun getPrediction(sign: String): PredictionModel?
 }
